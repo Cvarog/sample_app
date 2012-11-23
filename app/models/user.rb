01ae0853dbc94dcb0103afd82bf46303c0878beb
Cvarog <uchihamilord@gmail.com>
2012-11-23 17:03:45 +0400

    Added new model for indexing emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,13 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :name
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name, :presence => true
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
